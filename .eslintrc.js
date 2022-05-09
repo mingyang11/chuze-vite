653885d5a8dfcb13efabd352d4b6f7695da146bd
Mingyang11 <2578349261@qq.com>
2022-05-09 22:16:56 +0800

    eslint+prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,41 +1,35 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    // 从 ESLint 本身继承；
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    // 1.接入 prettier 的规则
+    'prettier',
+    'plugin:prettier/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended"
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react",
-        "@typescript-eslint"
-    ],
-    "rules": {
-        "indent": [
-            "error",
-            "tab"
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "double"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ]
-    }
-}
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  // 2.加入 prettier 的 eslint 插件
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {
+    // 3. 注意要加上这一句，开启 prettier 自动修复的功能
+    'prettier/prettier': 'error',
+    indent: ['error', 'tab'],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'double'],
+    semi: ['error', 'always'],
+    '@typescript-eslint/ban-ts-comment': 'error',
+    '@typescript-eslint/no-explicit-any': 'warn'
+  }
+};
