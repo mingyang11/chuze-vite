e8287307d80cc5314844ebef2950635756a17044
Mingyang11 <2578349261@qq.com>
2022-06-08 09:29:08 +0800

    提交

--- vite.config.ts
+++ vite.config.ts
@@ -28,7 +28,7 @@ export default defineConfig({
     react(),
     svgr(),
     myVitePlugin(),
-    virtual()
+    // virtual()
     // 图片压缩插件
     // viteImagemin({
     //   // 无损压缩配置，无损压缩下图片质量不会变差
