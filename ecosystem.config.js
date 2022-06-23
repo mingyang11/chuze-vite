03dd9911f9775549f0ab52f061687aacb5541140
Mingyang11 <2578349261@qq.com>
2022-06-23 22:46:10 +0800

    asf

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,7 +12,7 @@ module.exports = {
       ref: 'origin/master',
       repo: 'https://github.com/mingyang11/chuze-vite.git',
       path: '/frontcode/vite-demo',
-      'post-deploy': 'git reset --hard && git checkout master && git pull && npm i --production=false && npm run build:release && pm2 startOrReload ecosystem.config.js', // -production=false 下载全量包
+      'post-deploy': 'git reset --hard && git checkout master && git pull && npm i --production=false && pm2 startOrReload ecosystem.config.js', // -production=false 下载全量包
       env: {
         NODE_ENV: 'production'
       }
